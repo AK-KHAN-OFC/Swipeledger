89965577897767f6929892017a7ce6cfc7cb1560
bot998762 <khanofking637@gmail.com>
2026-08-26 04:02:17 +0530

    Phase 1 Update

--- client/src/services/api/client.js
+++ client/src/services/api/client.js
@@ -15,8 +15,8 @@
  */
 
 import axios from 'axios';
-import { getDeviceUUID } from '../utils/deviceUuid';
-import useAuthStore from '../store/authStore';
+import { getDeviceUUID } from '../../utils/deviceUuid';
+import useAuthStore from '../../store/authStore';
 
 const BASE_URL = import.meta.env.VITE_API_URL || '/api/v1';
 
